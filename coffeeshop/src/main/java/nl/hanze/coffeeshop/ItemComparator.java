10f931724f8e20c1646e055e26378b1eb0e4971b
jongsml <leon.jongsma@ndcmediagroep.nl>
2015-03-23 14:39:09 +0100

    cleanup

--- coffeeshop/src/main/java/nl/hanze/coffeeshop/ItemComparator.java
+++ coffeeshop/src/main/java/nl/hanze/coffeeshop/ItemComparator.java
@@ -4,15 +4,13 @@ import java.util.Comparator;
 
 public class ItemComparator implements Comparator<Item>
 { 
-	@Override 
-	public int compare(Item o1, Item o2) 
-	{
-		if (o1.getPrice() < o2.getPrice()) 
-			return -1;
-	    if (o1.getPrice() > o2.getPrice()) 
-	    	return 1;
-	    return 0;
-		
+  @Override 
+  public int compare(Item o1, Item o2) 
+  {
+    if (o1.getPrice() < o2.getPrice()) 
+      return -1;
+	if (o1.getPrice() > o2.getPrice()) 
+	  return 1;
+	return 0;	
 	}
-
 }
